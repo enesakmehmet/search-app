00f80608193d14258ce3d7acc8fd0b311f15f7b6
enesakmehmet <enesakmehmet7@gmail.com>
2024-12-19 22:11:39 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const formWrapper = document.querySelector(".form-wrapper");
+const form = document.querySelector("#form");
+const searchInput = document.querySelector("#searchInput");
+const buttonWrapper = document.querySelector(".button-wrapper");
+const searchButton = document.querySelector("#searchButton");
+const clearButton = document.querySelector("#clearButton");
+const imageListWrapper = document.querySelector(".imagelist-wrapper");
+
+runEventListeners();
+
+function runEventListeners() {
+    form.addEventListener("submit", search);
+    clearButton.addEventListener("click", clear);
+}
+
+function clear() {
+    searchInput.value = "";
+    Array.from(imageListWrapper.children).forEach((child) => child.remove());
+}
+
+function search(e) {
+    const value = searchInput.value.trim();
+    if (value === "") {
+        alert("Lütfen bir arama terimi girin.");
+        return;
+    }
+
+    fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
+        method: "GET",
+        headers: {
+            Authorization: "Client-ID C-ZwMdVfZLUqf2EUV6lJeOB9k0_1CVGHsXaUfwJRamU"
+        }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        clear(); // Önceki arama sonuçlarını temizle
+        Array.from(data.results).forEach((image) => {
+            addImageToUI(image.urls.small);
+        });
+    })
+    .catch((err) => console.log(err));
+
+    e.preventDefault();
+}
+
+function addImageToUI(url) {
+    const div = document.createElement("div");
+    div.className = "card";
+
+    const img = document.createElement("img");
+    img.setAttribute("src", url);
+    img.height = 400;
+    img.width = 400;
+
+    div.append(img);
+    imageListWrapper.append(div);
+}
